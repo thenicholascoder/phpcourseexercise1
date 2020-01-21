32a9b25f24d5dd7ddb3161fe8a9a102a52712fa4
thenicholascoder <thenicholashernandez@gmail.com>
2020-01-21 16:31:54 +0800

    initial upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	$firstName = "Nicholas";
+	$lastName = "Hernandez";
+	$fullName = $firstName." ".$lastName;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variable Quiz</title>
+</head>
+<body>
+<div>
+	<p>Create  2 variables one with your name another with your last name</p>
+
+	<p>Create a constant that eqals to both your names together</p>
+
+	<p>Echo that one out using h1 tag</p>
+
+	<p>Answer:</p>
+	<h1><?php echo $fullName; ?></h1>
+</div>
+
+</body>
+</html>
